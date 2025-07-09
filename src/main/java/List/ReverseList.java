43aa21249e60a9f5f3dde160617d7088d23df5aa
Betulazz <baihuadsb@gmail.com>
2025-07-09 22:08:35 +0800

    2025-07-09

---
+++ src/main/java/List/ReverseList.java
@@ -0,0 +1,58 @@
+package List;
+
+public class ReverseList {
+    public static void main(String[] args) {
+
+    }
+    //单链表
+    public static class ListNode{
+        int val;
+        ListNode next;
+        public ListNode(int val){
+            this.val = val;
+
+        }
+        public ListNode(int val,ListNode next){
+            this.val = val;
+            this.next=next;
+        }
+    }
+    //反转单链表
+    public static ListNode reverseList(ListNode head){
+        ListNode pre = null;
+        ListNode next = null;
+        while(head!=null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    //双链表
+    public static class DoubleListNode{
+        int val;
+        DoubleListNode last;
+        DoubleListNode next;
+        DoubleListNode(int val){
+            this.val=val;
+            this.last=null;
+            this.next=null;
+        }
+    }
+
+    //反转双链表
+    public static DoubleListNode reverseDoubleList(DoubleListNode head){
+        DoubleListNode pre = null;
+        DoubleListNode next = null;
+        while(head!=null){
+            next = head.next;
+            head.next = pre;
+            head.last = next;   //唯一区别
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
