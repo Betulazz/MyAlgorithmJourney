bbb8f2433bb702f81e4bf828c0e65287131b7860
Betulazz <baihuadsb@gmail.com>
2025-07-09 22:37:34 +0800

    2025-07-09

---
+++ src/main/java/List/MergeTwoLists.java
@@ -0,0 +1,33 @@
+package List;
+
+public class MergeTwoLists {
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+    public static ListNode mergeTwoLists(ListNode head1,ListNode head2){
+        //head节点为最小的节点
+        ListNode head = head1.val<=head2.val ? head1 : head2;
+        ListNode cur1 = head.next;
+        ListNode cur2 = head==head1 ? head2 : head1;
+        //pre代表未排序的链表节点的前一个，即已排序链表的最后一个
+        ListNode pre = head;
+        while(cur1!=null && cur2!=null){
+            if(cur1.val<=cur2.val){
+                pre.next = cur1;
+                cur1 = cur1.next;
+            }
+            else{
+                pre.next = cur2;
+                cur2 = cur2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = cur1==null ? cur2 : cur1;
+        return head;
+    }
+}
