7a6f8b8acc517db48fc754e03632fd058328e3e4
Betulazz <baihuadsb@gmail.com>
2025-07-16 23:50:59 +0800

    7.16

---
+++ src/main/java/BinaryTree/BinaryTreeTraversalRecursion.java
@@ -0,0 +1,65 @@
+package BinaryTree;
+
+public class BinaryTreeTraversalRecursion {
+    public static class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    //先序遍历
+    public static void preOrder(TreeNode head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.val + " ");
+        preOrder(head.left);
+        preOrder(head.right);
+    }
+
+    //中序遍历
+    public static void inOrder(TreeNode head) {
+        if (head == null) {
+            return;
+        }
+        inOrder(head.left);
+        System.out.print(head.val + " ");
+        inOrder(head.right);
+    }
+
+    //后序遍历
+    public static void posOrder(TreeNode head) {
+        if (head == null) {
+            return;
+        }
+        posOrder(head.left);
+        posOrder(head.right);
+        System.out.print(head.val + " ");
+    }
+
+    public static void main(String[] args) {
+        TreeNode head = new TreeNode(1);
+        head.left = new TreeNode(2);
+        head.right = new TreeNode(3);
+        head.left.left = new TreeNode(4);
+        head.left.right = new TreeNode(5);
+        head.right.left = new TreeNode(6);
+        head.right.right = new TreeNode(7);
+
+        System.out.println("树的先序遍历：");
+        preOrder(head);
+        System.out.println();
+
+        System.out.println("树的中序遍历：");
+        inOrder(head);
+        System.out.println();
+
+        System.out.println("树的后序遍历：");
+        posOrder(head);
+        System.out.println();
+    }
+}
